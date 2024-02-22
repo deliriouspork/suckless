72b270f5d041d85ddf2dfddbbb8a2ef724faa215
deliriouspork <depk28@mailbox.org>
2024-02-21 19:47:04 -0800

    add lock and firefox logos to tags 8 and 9

--- dwm/config.h
+++ dwm/config.h
@@ -6,7 +6,7 @@ static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "SourceCodePro:size=12" };
+static const char *fonts[]          = { "SourceCodePro:size=12", "Font Awesome:size=12" };
 static const char dmenufont[]       = "SourceCodePro:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#716BB0";
@@ -20,7 +20,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
